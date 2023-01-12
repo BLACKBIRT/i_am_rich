91b29c333ff3c3f57f9ff0c24ae3cc12376fd416
BLACKBIRT <mauropian@yahoo.gr>
2023-01-12 19:48:25 +0200

    Add files via upload

---
+++ lib/main.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(
+    MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.blueGrey[200],
+        appBar: AppBar(
+          title: Text('I Am Rich'),
+          backgroundColor: Colors.blueGrey[900],
+        ),
+        body: Center(
+          child: Image(
+            image: AssetImage('images/diamond.png'),
+          ),
+          ),
+        ),
+        ),
+  );
+}
